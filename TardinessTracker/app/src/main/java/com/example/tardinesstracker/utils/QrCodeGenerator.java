b459aaa32ad2943f3315cd11765ed7735add85ea
Aldick22 <du228su@gmail.com>
2025-04-05 09:39:57 +0500

    Update QrCodeGenerator.java

--- TardinessTracker/app/src/main/java/com/example/tardinesstracker/utils/QrCodeGenerator.java
+++ TardinessTracker/app/src/main/java/com/example/tardinesstracker/utils/QrCodeGenerator.java
@@ -1,4 +1,43 @@
 package com.example.tardinesstracker.utils;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
 public class QrCodeGenerator {
+    
+    /**
+     * Generates a QR code bitmap from student data
+     * @param studentId The student ID
+     * @param fullName The student's full name
+     * @param width Width of the QR code
+     * @param height Height of the QR code
+     * @return Bitmap containing the QR code
+     */
+    public static Bitmap generateQrCode(int studentId, String fullName, int width, int height) {
+        try {
+            // Create data string to encode
+            String qrData = "STUDENT:" + studentId + ":" + fullName;
+            
+            QRCodeWriter writer = new QRCodeWriter();
+            BitMatrix bitMatrix = writer.encode(qrData, BarcodeFormat.QR_CODE, width, height);
+            
+            // Convert bit matrix to bitmap
+            Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bitmap;
+            
+        } catch (WriterException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
