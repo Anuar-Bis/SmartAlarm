bdd7812e4fe098046be3b4d0a9e4cd22dab0405c
Anuar-Bis <bisenov2008@gmail.com>
2025-04-05 09:35:44 +0500

    Update ParentMainActivity.java

--- TardinessTracker/app/src/main/java/com/example/tardinesstracker/ui/parent/ParentMainActivity.java
+++ TardinessTracker/app/src/main/java/com/example/tardinesstracker/ui/parent/ParentMainActivity.java
@@ -1,4 +1,24 @@
 package com.example.tardinesstracker.ui.parent;
 
-public class ParentMainActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.tardinesstracker.R;
+
+public class ParentMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_main);
+
+        // Load default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.parent_fragment_container, new ChatbotFragment())
+                    .commit();
+        }
+    }
 }
