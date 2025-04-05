ced5ccac6331b801abc8821aa82466fca84ba9f7
Aldick22 <du228su@gmail.com>
2025-04-05 09:21:56 +0500

    Update TeacherDao.java

--- TardinessTracker/app/src/main/java/com/example/tardinesstracker/data/dao/TeacherDao.java
+++ TardinessTracker/app/src/main/java/com/example/tardinesstracker/data/dao/TeacherDao.java
@@ -1,4 +1,40 @@
 package com.example.tardinesstracker.data.dao;
 
-public class TeacherDao {
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.tardinesstracker.data.entities.Teacher;
+
+import java.util.List;
+
+@Dao
+public interface TeacherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long insertTeacher(Teacher teacher);
+    
+    @Update
+    void updateTeacher(Teacher teacher);
+    
+    @Delete
+    void deleteTeacher(Teacher teacher);
+    
+    @Query("SELECT * FROM teachers WHERE id = :id")
+    LiveData<Teacher> getTeacherById(int id);
+    
+    @Query("SELECT * FROM teachers")
+    LiveData<List<Teacher>> getAllTeachers();
+    
+    @Query("SELECT * FROM teachers WHERE school = :school")
+    LiveData<List<Teacher>> getTeachersBySchool(String school);
+    
+    @Query("SELECT * FROM teachers WHERE className = :className")
+    LiveData<List<Teacher>> getTeachersByClass(String className);
+    
+    @Query("SELECT * FROM teachers WHERE email = :email AND password = :password LIMIT 1")
+    Teacher login(String email, String password);
 }
